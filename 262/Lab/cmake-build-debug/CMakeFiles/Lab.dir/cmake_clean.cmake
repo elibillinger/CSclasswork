8b32788f45175241fd8bcdd8f7de0e37c7bfc72f
elibillinger <eebillin@hotmail.com>
2021-08-25 18:45:18 -0600

    Add Lab 1 and 2

---
+++ 262/Lab/cmake-build-debug/CMakeFiles/Lab.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab.dir/main.cpp.o"
+  "Lab"
+  "Lab.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 262/lab2/cmake-build-debug/CMakeFiles/mondrian.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mondrian.dir/lodepng.cpp.o"
+  "CMakeFiles/mondrian.dir/main.cpp.o"
+  "CMakeFiles/mondrian.dir/mondrian.cpp.o"
+  "CMakeFiles/mondrian.dir/picture.cpp.o"
+  "mondrian"
+  "mondrian.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mondrian.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

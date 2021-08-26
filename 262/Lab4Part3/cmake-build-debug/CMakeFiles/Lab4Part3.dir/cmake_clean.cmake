fd1c3610a3ca25e2f0d77f304d851d1a723da6ee
elibillinger <eebillin@hotmail.com>
2021-08-25 18:46:49 -0600

    Added Lab 3 and 4

---
+++ 262/Lab4Part3/cmake-build-debug/CMakeFiles/Lab4Part3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab4Part3.dir/main.cpp.o"
+  "Lab4Part3"
+  "Lab4Part3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab4Part3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 262/lab03/cmake-build-debug/CMakeFiles/rwg.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rwg.dir/main.cpp.o"
+  "CMakeFiles/rwg.dir/rwg.cpp.o"
+  "rwg"
+  "rwg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rwg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
